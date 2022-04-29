64b1b92e3047b429eddfe931d2170aba21535f7f
Shadow1595 <luisc6646@gmail.com>
2022-04-28 21:55:07 -0500

    articulos

--- index.php
+++ index.php
@@ -43,69 +43,35 @@
 			]);
 
 		while ($articulos->have_posts()) {
-		$articulos->the_post();
-
-		the_title();
-		echo '<hr>';
-		} ?>
+		$articulos->the_post(); ?>
 
 
 
 
 
 
-	<h2>Guerra Rusia Vs Ucrania</h2>
+
+	
+		<h2><?php the_title(); ?></h2>
 	<div class="row">
 		<div class="col-sm-5">
-	<img src="https://i.ytimg.com/vi/Y4pwowUVusQ/maxresdefault.jpg"class="ucrania img-fluid">
+	<?php the_post_thumbnail("medium"); ?>
 		</div>
 		<div class="col-sm-7">
-		<p>Ucrania ha recibido este jueves nuevos compromisos en su resistencia contra la guerra de Rusia. Joe Biden ha prometido 800 millones de dólares adicionales en ayuda militar y Pedro Sánchez, que ha visitado Kiev, ha anunciado el mayor envío hasta ahora de armamento español y la llegada de agentes para investigar los crímenes de guerra. En paralelo, Kiev trabaja en un plan para que fondos congelados a Moscú, como parte de las sanciones, sirvan para pagar la reconstrucción tras la invasión, según avanza Reuters.
-
-		Sobre el terreno, la situación de Mariupol es objeto de versiones diferentes. Aunque Putin ha celebrado la toma de la ciudad, pese a haber renunciado a asaltar la planta donde resisten combatientes ucranianos, fuentes de este país y el presidente estadounidense Biden dudan de que tenga el control total sobre esta localidad de gran importancia estratégica.</p>
+			<?php the_excerpt(); ?>
 		</div>
 	</div>
 
-	<h3>Peru vs Paraguay</h3>
-	<div class="row">
+
+
+
+
+
+		<?php } ?>
+
 		
-	<div class="col-sm-7">
-		<p>
-		Perú vs. Paraguay 2-0. La selección peruana logró un importante triunfo y logró la codiciada plaza del repechaje al Mundial Qatar 2022 en el decisivo partido por la última fecha de la clasificatoria sudamericana. El duelo se jugó en el Estadio Nacional de Lima que por primera vez tras dos años de pandemia lució lleno total de hinchas que alentaron a la bicolor de principio a fin. Esta nueva alegría de la bicolor nos manda a Qatar y ahora tendrá que esperar a su próximo rival que saldrá del Australia vs Emiratos Arabes..
 
-		En el primer tiempo, los dirigidos por Ricardo Gareca salieron con todo para asegurar el triunfo que les asegure el boleto al repechaje con el representante de Asia. Así, a los 4 minutos del iniciado el juego, Gianluca Lapadula hizo saltar a todo el Perú de sus asientos y gritar a todo pulmón el 1-0 a favor.
-
-		En los descuentos, una jugada de Gianluca Lapadula pudo ser el tercero para los peruanos, pero el palo le dijo que no, pero en el rebote, Callens no la agarró bien y el disparo salió desviado.
-
-		Un segundo tiempo sin mucho apuro, la selección peruana protegió su resultado y así el estadio Nacional cantó a viva voz ‘cómo no te voy a querer’ tras el pitazo final que desató la euforia de todos los integrantes de la selección peruana
-	</p>
-	</div>
-	<div class="col-sm-5">
-			<img src="https://phantom-marca.unidadeditorial.es/addff6e0d53bf87117cd6253e78a4107/resize/1320/f/jpg/assets/multimedia/imagenes/2022/03/28/16484899804985.jpg" class="partido img-fluid">
-		</div>
-</div>
-		
-	<h2>Covid-19</h2>	
-	<div class="row">
-		<div class="col-sm-5">
-			<img src="https://www.paho.org/sites/default/files/styles/max_1500x1500/public/2021-05/covid-19-variants.jpg?itok=szJH1mCw" class="img-fluid">
-		</div>
-	<div class="col-sm-7">
-		<p>
-		La COVID-19 afecta de distintas maneras en función de cada persona. La mayoría de las personas que se contagian presentan síntomas de intensidad leve o moderada, y se recuperan sin necesidad de hospitalización.
-		Los síntomas más habituales son los siguientes:
-		Fiebre
- 		Tos
-		Cansancio
-		Pérdida del gusto o del olfato
-		Los síntomas menos habituales son los siguientes:
-		Dolor de garganta
-		Dolor de cabeza
-		Molestias y dolores
-		Diarrea
-		Erupción cutánea o pérdida del color de los dedos de las manos o los pies
-		Ojos rojos o irritados
-	</div>
+	
 
 
 	</center>
